73909ede6b4175d6de6d2b94c450ac74b5748d1d
piotrkedra <pkedra@codenauts.io>
2026-04-14 07:47:31 +0200

    refactor:adjust naming conventions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { HeroSection } from '@/features/landing/hero-section';
+import { HeroSection } from '@/features/landing/HeroSection';
 
 export default function Home() {
   return <HeroSection />;
